fa836d401cf8010cc9c0fdb7e5c2d33ba2767bdb
saunderscox <saunderscox@gmail.com>
2025-08-15 10:37:48 -0400

    added pages & routing for home, about, contact, not-found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./features/home/home').then((m) => m.Home),
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./features/about/about').then((m) => m.About),
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./features/contact/contact').then((m) => m.Contact),
+  },
+  {
+    path: 'not-found',
+    loadComponent: () =>
+      import('./features/not-found/not-found').then((m) => m.NotFound),
+  },
+];
